672f5cb34e49149ba08c75ba18eff94278859deb
kopy-kat <kopp.konrad@gmail.com>
2023-12-15 12:45:09 +0100

    feat: first version without content

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,44 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import { LiaTelegram } from "react-icons/lia";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span style={{ fontWeight: "bold" }}>ERC-7579</span>,
+  useNextSeoProps: () => {
+    return {
+      titleTemplate: "%s – ERC-7579",
+    };
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="ERC-7579" />
+      <meta
+        property="og:description"
+        content="Minimal Modular Smart Accounts"
+      />
+    </>
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/erc7579",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://t.me/+KfB9WuhKDgk5YzIx",
+    icon: <LiaTelegram size={24} style={{ marginLeft: "-10px" }} />,
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/erc7579/docs/tree/main",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "ERC-7579 Docs",
   },
-}
+  banner: {
+    key: "initial-release",
+    text: (
+      <a href="https://github.com/ethereum/ERCs/pull/163/files" target="_blank">
+        Check out the first draft of ERC-7579 →
+      </a>
+    ),
+  },
+  faviconGlyph: "📝",
+};
 
-export default config
+export default config;
